b9fee0db29b3ba2f92456d2d7ee690a63a9a5480
Eoin Landy <info@tuarua.co.uk>
2020-09-26 11:30:42 +0100

    use air-tools

--- example/get_android_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-AneVersion="1.5.0"
-FreKotlinVersion="1.40.0"
-
-wget -O android_dependencies/com.tuarua.frekotlin-$FreKotlinVersion.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
-wget -O ../native_extension/ane/DeviceInfoANE.ane https://github.com/tuarua/DeviceInfo-ANE/releases/download/$AneVersion/DeviceInfoANE.ane?raw=true
